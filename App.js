0cdf2f5700c15d70102762f55396cb5fd2de5d29
FelixAshong <fnoashong@st.ug.edu.gh>
2025-01-30 02:10:30 +0000

    commit

--- App.js
+++ App.js
@@ -1,21 +1,30 @@
 import React from 'react';
-import { SafeAreaView, StyleSheet, StatusBar } from 'react-native';
+import { SafeAreaView, StyleSheet, StatusBar, LogBox } from 'react-native';
 import AppNavigator from './navigation/AppNavigator';
-import { AppProvider } from './context/AppContext'; // Correct import path
+import { AppProvider } from './context/AppContext';
+import { configureReanimatedLogger, ReanimatedLogLevel } from 'react-native-reanimated';
 
+// Disable specific warnings
+LogBox.ignoreLogs([
+  '[Reanimated] Reading from `value` during component render',
+]);
 
+// Disable Reanimated strict mode warnings
+configureReanimatedLogger({
+  level: ReanimatedLogLevel.warn,
+  strict: false, // Disable strict mode
+});
 
 export default function App() {
   return (
-    <SafeAreaView style={styles.container}>
-      <StatusBar barStyle="dark-content" backgroundColor="#FFFFFF" />
-      <AppProvider>
+    <AppProvider>
+      <SafeAreaView style={styles.container}>
+        <StatusBar barStyle="dark-content" backgroundColor="#FFFFFF" />
         <AppNavigator />
-      </AppProvider>
-    </SafeAreaView>
+      </SafeAreaView>
+    </AppProvider>
   );
 }
-
 
 const styles = StyleSheet.create({
   container: {
@@ -23,4 +32,3 @@ const styles = StyleSheet.create({
     backgroundColor: '#FFFFFF',
   },
 });
-
